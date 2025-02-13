7a042d3ae4ffa17eca3518cdb1924fd648cad4f7
milanuhrin <milan.uhrin19@gmail.com>
2025-02-13 10:19:51 +0100

    CORS update

--- amplify/backend/function/updateAvailability/src/app.js
+++ amplify/backend/function/updateAvailability/src/app.js
@@ -12,9 +12,10 @@ app.use(bodyParser.json());
 app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS"); 
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
